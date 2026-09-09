dd90da467a5721466604eb14fd6840a4b65d2b9e
ScrappyAT <113542602+ScrappyAT@users.noreply.github.com>
2026-09-09 17:05:01 +0100

    chore: tunnel dev origin, and evidence of duplicate webhook handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['possibilities-else-round-kurt.trycloudflare.com'],
 };
 
 export default nextConfig;
